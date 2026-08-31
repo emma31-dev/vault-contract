b7b0a0a68930c98aabf9d3a76569b6d67984c8e0
emma31-dev <emmanuelfidel07@gmail.com>
2026-08-31 16:59:09 +0100

    Add SafeERC20 library usage for IERC20

--- src/Vault.sol
+++ src/Vault.sol
@@ -32,6 +32,8 @@ interface IERC4626 is IERC20 {
 }
 
 contract Vault is IERC4626, Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     address public immutable override asset;
     uint256 private _totalShares;
     mapping(address => uint256) private _shares;
